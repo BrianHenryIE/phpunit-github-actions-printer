ad0ff04f0f4e2bd88eee5b65130d295d71b8201e
Brian Henry <BrianHenryIE@gmail.com>
2020-03-22 18:36:06 -0700

    pp

--- src/Printer.php
+++ src/Printer.php
@@ -65,7 +65,7 @@ class Printer extends ResultPrinter
         $type = $this->getCurrentType();
         $file = "file={$this->relativePath($path)}";
         $line = "line={$line}";
-        $this->write("::{$type} $file,$line::{$message}\n \${{ GITHUB_WORKSPACE }} \n /blob/ \n \${{ GITHUB_SHA }} \n");
+        $this->write("::{$type} $file,$line::{$message}  \${{ GITHUB_WORKSPACE }}  /blob/  \${{ GITHUB_SHA }} \n");
     }
 
     protected function getCurrentType()
